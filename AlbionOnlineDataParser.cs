8ea39404d4d67fdb259f7f408ab8b4eee3e8041b
Jaysen Becerra <becerraj27@gmail.com>
2022-09-28 07:59:15 -0500

    Adding player data handler and albion online parser

---
+++ AlbionOnlineDataParser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+
+namespace AlbionOnlineDataParser
+{
+    public static class AlbionOnlineDataParser
+    {
+        public static HttpClient ApiClient { get; set; } = new HttpClient();
+
+        public enum AlbionAPIDataTypesEnum
+        {
+            search,
+            playerSearch,
+            playerDeaths,
+            playerKills,
+            playerStatistics,
+            events
+
+        }
+
+
+        public static void InitializeClient()
+        {
+            ApiClient = new HttpClient();
+            ApiClient.BaseAddress = new Uri("https://gameinfo.albiononline.com/api/gameinfo/");
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+    }
+}
