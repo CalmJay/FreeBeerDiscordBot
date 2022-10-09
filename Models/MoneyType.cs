b901c32ae742bdc8bbbbf73e46dc39d69e075df9
Murad <Murad@DESKTOP-UMEK5DQ>
2022-10-09 10:36:23 +0300

    Database  Models V1

---
+++ Models/MoneyType.cs
@@ -0,0 +1,35 @@
+﻿using DiscordBot.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace DiscordBot.Models
+{
+    public partial class MoneyType
+    {
+        public MoneyType()
+        {
+            PlayerLoot = new HashSet<PlayerLoot>();
+        }
+
+        public int Id { get; set; }
+        //public string Type
+        //{
+        //    get
+        //    {
+        //        return this.Types.ToString();
+        //    }
+        //    set
+        //    {
+        //        Type = value;
+        //    }
+        //}
+        [EnumDataType(typeof(MoneyTypes))]
+        public MoneyTypes Type { get; set; }
+        public virtual ICollection<PlayerLoot> PlayerLoot { get; set; }
+    }
+}
