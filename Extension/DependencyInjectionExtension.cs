89e163cf0ec6ff6f80341ae1db6f9136f0dd6e41
Murad <Murad@DESKTOP-UMEK5DQ>
2022-10-20 15:26:15 +0300

    ....

---
+++ Extension/DependencyInjectionExtension.cs
@@ -0,0 +1,20 @@
+﻿using DiscordBot.Models;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscordBot.Extension
+{
+    public static class DependencyInjectionExtension
+    {
+
+        public static void DependencyInjection(ServiceCollection services)
+        {
+
+            //DbContext
+            services.AddTransient<FreeBeerdbContext>();
+
+        }
+    }
+}
