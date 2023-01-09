5ed19f093226f5ca0e66c263f8c6d2b9795c37a8
BecerraJ <becerraj27@gmail.com>
2023-01-08 18:22:06 -0600

    Bug fixes and cleanup

--- CommandModule.cs
+++ CommandModule.cs
@@ -16,16 +16,10 @@ using DiscordBot.RegearModule;
 using MarketData;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
-using System.IO;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
-using System.Runtime.CompilerServices;
-using System.Collections.ObjectModel;
 using Microsoft.EntityFrameworkCore.Internal;
 
 namespace CommandModule
 {
-    // Must use InteractionModuleBase<SocketInteractionContext> for the InteractionService to auto-register the commands
     public class CommandModule : InteractionModuleBase<SocketInteractionContext>
     {
         public InteractionService Commands { get; set; }
@@ -38,72 +32,6 @@ namespace CommandModule
             _logger = logger;
         }
 
-        // Simple slash command to bring up a message with a button to press
-        //[SlashCommand("button", "Button demo command")]
-        //public async Task ButtonInput()
-        //{
-        //    var components = new ComponentBuilder();
-        //    var button = new ButtonBuilder()
-        //    {
-        //        Label = "Button",
-        //        CustomId = "button1",
-        //        Style = ButtonStyle.Primary
-        //    };
-
-        //    // Messages take component lists. Either buttons or select menus. The button can not be directly added to the message. It must be added to the ComponentBuilder.
-        //    // The ComponentBuilder is then given to the message components property.
-        //    components.WithButton(button);
-
-        //    await RespondAsync("This message has a button!", components: components.Build());
-        //}
-
-        //// This is the handler for the button created above. It is triggered by nmatching the customID of the button.
-        //[ComponentInteraction("button1")]
-        //public async Task ButtonHandler()
-        //{
-        //    // try setting a breakpoint here to see what kind of data is supplied in a ComponentInteraction.
-        //    var c = Context;
-
-        //    await RespondAsync($"You pressed a button!");
-        //}
-
-        //// Simple slash command to bring up a message with a select menu
-        //[SlashCommand("menu", "Select Menu demo command")]
-        //public async Task MenuInput()
-        //public async Task MenuInput()
-        //{
-        //    var components = new ComponentBuilder();
-        //    // A SelectMenuBuilder is created
-        //    var select = new SelectMenuBuilder()
-        //    {
-        //        CustomId = "menu1",
-        //        Placeholder = "Select something"
-        //    };
-        //    // Options are added to the select menu. The option values can be generated on execution of the command. You can then use the value in the Handler for the select menu
-        //    // to determine what to do next. An example would be including the ID of the user who made the selection in the value.
-        //    select.AddOption("abc", "abc_value");
-        //    select.AddOption("def", "def_value");
-        //    select.AddOption("ghi", "ghi_value");
-
-        //    components.WithSelectMenu(select);
-
-        //    await RespondAsync("This message has a menu!", components: components.Build());
-        //}
-
-        //// SelectMenu interaction handler. This receives an array of the selections made.
-        //[ComponentInteraction("menu1")]
-        //public async Task MenuHandler(string[] selections)
-        //{
-        //    // For the sake of demonstration, we only want the first value selected.
-        //    await RespondAsync($"You selected {selections.First()}");
-        //}
-
-        //[SlashCommand("split-loot", "Automated Split loot")]
-        //public async Task SplitLoot(IAttachment a_iImageAttachment)
-        //{
-
-
-        //}
         [SlashCommand("get-player-info", "Search for Player Info")]
         public async Task GetBasicPlayerInfo(string a_sPlayerName)
         {
@@ -442,7 +370,6 @@ namespace CommandModule
             if (guildUser.Roles.Any(r => r.Name == "AO - REGEARS" || r.Name == "AO - Officers"))
             {
                 dataBaseService = new DataBaseService();
-                dataBaseService.DeletePlayerLootByKillId(killId.ToString());
 
                 try
                 {
@@ -481,15 +408,16 @@ namespace CommandModule
             if (guildUser.Roles.Any(r => r.Name == "AO - REGEARS" || r.Name == "AO - Officers"))
             {
                 PlayerEventData = await eventData.GetAlbionEventInfo(killId);
-                await GoogleSheetsDataWriter.WriteToRegearSheet(Context, PlayerEventData, refundAmount, callername);
+                await GoogleSheetsDataWriter.WriteToRegearSheet(Context, PlayerEventData, refundAmount, callername, MoneyTypes.ReGear);
                 await Context.Channel.DeleteMessageAsync(interaction.Message.Id);
 
-                await Context.Guild.GetUser(regearPoster).SendMessageAsync($"<@{Context.Guild.GetUser(regearPoster).Id}> your regear https://albiononline.com/en/killboard/kill/{killId} has been approved! ${refundAmount.ToString("N0")} has been added to your paychex");
-
+                
                 if (Context.Guild.GetUser(regearPoster).Roles.Any(r => r.Name == "Free Regear - Eligible"))
                 {
-                    await Context.Guild.GetUser(regearPoster).RemoveRoleAsync(1052241667329118349);//using test role
+                    await Context.Guild.GetUser(regearPoster).RemoveRoleAsync(1052241667329118349);
                 }
+
+                await Context.Guild.GetUser(regearPoster).SendMessageAsync($"<@{Context.Guild.GetUser(regearPoster).Id}> your regear https://albiononline.com/en/killboard/kill/{killId} has been approved! ${refundAmount.ToString("N0")} has been added to your paychex");
 
                 await _logger.Log(new LogMessage(LogSeverity.Info, "Regear Approved", $"User: {Context.User.Username}, Approved the regear {killId} for {victimName} ", null));
             }
